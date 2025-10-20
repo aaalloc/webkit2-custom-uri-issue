0311314428d2a26bdc270d452de9330dc18ab7ef
Siirko <contact@yanovskyy.com>
2025-10-20 22:33:41 +0200

    working example with any type of path TODO: make the plugin now behave as a proxy

--- gst_custom_uri.c
+++ gst_custom_uri.c
@@ -6,6 +6,9 @@
 
 #define ASSET_URI_STR "assets"
 
+GstElement *pipeline, *sink;
+
+
 static GstURIType
 gst_assets_video_src_uri_get_type (GType type)
 {
@@ -41,11 +44,6 @@ gst_assets_video_src_uri_set_uri (GstURIHandler * handler, const gchar * uri, GE
   if (!uri || !g_str_has_prefix(uri, "assets://"))
     return FALSE;
 
-  // convert "assets://file.mp4" → "file:///opt/app/assets/file.mp4"
-  const gchar *filename = uri + strlen("assets://");
-  gchar *real_uri = g_strdup_printf("file:///opt/app/assets/%s", filename);
-  g_object_set(self, "location", real_uri, NULL);
-  g_free(real_uri);
   return TRUE;
 }
 
@@ -80,30 +78,14 @@ G_DEFINE_TYPE_WITH_CODE (GstRedVideoSrc, gst_assets_video_src,
 static GstFlowReturn
 gst_assets_video_src_create (GstPushSrc * src, GstBuffer ** p_buf)
 {
-  GstBuffer *buf;
-  GstMapInfo map;
-  guint w = 64, h = 64;
-  guint size;
-
-  size = w * h * 3 / 2;
-  buf = gst_buffer_new_and_alloc (size);
-  gst_buffer_map (buf, &map, GST_MAP_WRITE);
-  memset (map.data, 76, w * h);
-  memset (map.data + (w * h), 85, (w * h) / 4);
-  memset (map.data + (w * h) + ((w * h) / 4), 255, (w * h) / 4);
-  gst_buffer_unmap (buf, &map);
-
-  *p_buf = buf;
+  // TODO: goal here is just get buffer from pad
   return GST_FLOW_OK;
 }
 
 static GstCaps *
 gst_assets_video_src_get_caps (GstBaseSrc * src, GstCaps * filter)
 {
-  guint w = 64, h = 64;
-  return gst_caps_new_simple ("video/x-raw", "format", G_TYPE_STRING,
-      "I420", "width", G_TYPE_INT, w, "height",
-      G_TYPE_INT, h, "framerate", GST_TYPE_FRACTION, 1, 1, NULL);
+  return gst_caps_new_any();
 }
 
 static void
@@ -133,7 +115,7 @@ gst_assets_video_src_init (GstRedVideoSrc * src)
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
-  if (!gst_element_register (plugin, "redvideosrc", GST_RANK_PRIMARY,
+  if (!gst_element_register (plugin, "assetsrc", GST_RANK_PRIMARY,
           gst_assets_video_src_get_type ())) {
     return FALSE;
   }
@@ -141,67 +123,148 @@ plugin_init (GstPlugin * plugin)
 }
 
 
-#define PACKAGE "RedVideoSrc"
+#define PACKAGE "CustomUriProxy"
 
 GST_PLUGIN_DEFINE
     (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    redvideosrc,
-    "Red Video Source",
+    assetsrc,
+    "Plugin proxy PoC",
     plugin_init,
     "1.0", "LGPL", "GStreamer", "http://gstreamer.net/")
 
 
 
-int main(int argc, char *argv[]) {
-    GstElement *pipeline;
-    GError *error = NULL;
+static void
+cb_pad_added (GstElement *dec,
+          GstPad     *pad,
+          gpointer    data)
+{
+  GstCaps *caps;
+  GstStructure *str;
+  const gchar *name;
+  GstPadTemplate *templ;
+  GstElementClass *klass;
 
-    gst_init(&argc, &argv);
+  /* check media type */
+  caps = gst_pad_query_caps (pad, NULL);
+  str = gst_caps_get_structure (caps, 0);
+  name = gst_structure_get_name (str);
 
-    if (!gst_plugin_register_static(GST_VERSION_MAJOR, GST_VERSION_MINOR,
-    "redvideosrc", "Red Video Source", plugin_init,
-    "1.0", "LGPL", "GStreamer", "http://gstreamer.net/", "source"))
-    {
-        g_printerr("Failed to register redvideosrc plugin\n");
-        return -1;
+  klass = GST_ELEMENT_GET_CLASS (sink);
+
+  if (g_str_has_prefix (name, "audio")) {
+    templ = gst_element_class_get_pad_template (klass, "audio_sink");
+  } else if (g_str_has_prefix (name, "video")) {
+    templ = gst_element_class_get_pad_template (klass, "video_sink");
+  } else if (g_str_has_prefix (name, "text")) {
+    templ = gst_element_class_get_pad_template (klass, "text_sink");
+  } else {
+    templ = NULL;
+  }
+
+  if (templ) {
+    GstPad *sinkpad;
+
+    sinkpad = gst_element_request_pad (sink, templ, NULL, NULL);
+
+    if (!gst_pad_is_linked (sinkpad))
+      gst_pad_link (pad, sinkpad);
+
+    gst_object_unref (sinkpad);
+  }
+
+  gst_clear_caps (&caps);
+}
+static GMainLoop *loop;
+
+static gboolean
+my_bus_callback (GstBus * bus, GstMessage * message, gpointer data)
+{
+  // g_print ("Got %s message\n", GST_MESSAGE_TYPE_NAME (message));
+  GError *err;
+  gchar *debug;
+  switch (GST_MESSAGE_TYPE (message)) {
+    case GST_MESSAGE_ERROR:{
+      gst_message_parse_error (message, &err, &debug);
+      g_print ("Error: %s\n", err->message);
+      g_error_free (err);
+      g_free (debug);
+
+      g_main_loop_quit (loop);
+      break;
     }
-
-    if (argc != 2) {
-        g_printerr("Usage: %s assets://filename\n", argv[0]);
-        return -1;
+    case GST_MESSAGE_WARNING:{
+      gst_message_parse_warning (message, &err, &debug);
+      g_print ("Warning: %s\n", err->message);
+      g_error_free (err);
+      g_free (debug);
+      break;
     }
+    case GST_MESSAGE_EOS:
+      /* end-of-stream */
+      g_main_loop_quit (loop);
+      break;
+    default:
+      /* unhandled message */
+      break;
+  }
 
-    gchar *uri = argv[1];
-    pipeline = gst_element_make_from_uri(GST_URI_SRC, uri, "source", &error);
-    if (!pipeline) {
-        g_printerr("Could not create source from URI: %s\n", error->message);
-        g_clear_error(&error);
-        return -1;
-    }
+  /* we want to be notified again the next time there is a message
+   * on the bus, so returning TRUE (FALSE means we want to stop watching
+   * for messages on the bus and our callback should not be called again)
+   */
+  return TRUE;
+}
 
-    gchar *pipeline_desc = g_strdup_printf("uridecodebin uri=%s ! autovideosink", uri);
-    pipeline = gst_parse_launch(pipeline_desc, &error);
-    if (!pipeline) {
-        g_printerr("Pipeline creation failed: %s\n", error->message);
-        g_clear_error(&error);
-        return -1;
-    }
-    g_free(pipeline_desc);
+gint
+main (gint   argc,
+      gchar *argv[])
+{
+  GstElement *dec;
+  GstBus *bus;
 
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    g_print("Playing %s\n", uri);
+  /* init GStreamer */
+  gst_init (&argc, &argv);
 
-    GstBus *bus = gst_element_get_bus(pipeline);
-    GstMessage *msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
-                                                 GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+  if (!gst_plugin_register_static(GST_VERSION_MAJOR, GST_VERSION_MINOR,
+  "assetsrc", "Red Video Source", plugin_init,
+  "1.0", "LGPL", "GStreamer", "http://gstreamer.net/", "source"))
+  {
+      g_printerr("Failed to register assetsrc plugin\n");
+      return -1;
+  }
 
-    if (msg != NULL)
-        gst_message_unref(msg);
+  loop = g_main_loop_new (NULL, FALSE);
 
-    gst_object_unref(bus);
-    gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
+  /* make sure we have input */
+  if (argc != 2) {
+    g_print ("Usage: %s <uri>\n", argv[0]);
+    return -1;
+  }
 
-    return 0;
+  /* setup */
+  pipeline = gst_pipeline_new ("pipeline");
+
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+  gst_bus_add_watch (bus, my_bus_callback, loop);
+  gst_object_unref (bus);
+
+  dec = gst_element_factory_make ("uridecodebin", "source");
+  g_object_set (G_OBJECT (dec), "uri", argv[1], NULL);
+  g_signal_connect (dec, "pad-added", G_CALLBACK (cb_pad_added), NULL);
+
+  /* create audio output */
+  sink = gst_element_factory_make ("playsink", "sink");
+  gst_bin_add_many (GST_BIN (pipeline), dec, sink, NULL);
+
+  /* run */
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  g_main_loop_run (loop);
+
+  /* cleanup */
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (GST_OBJECT (pipeline));
+
+  return 0;
 }
