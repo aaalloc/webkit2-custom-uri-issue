3e571a9eeb0c0f11ee7ecbd7a089bf3f563ae648
Siirko <contact@yanovskyy.com>
2025-10-27 22:53:32 +0100

    it works !

--- gst_assets.h
+++ gst_assets.h
@@ -1,198 +1,193 @@
 #pragma once
 
-#include <gst/check/gstcheck.h>
 #include <gst/base/gstpushsrc.h>
+#include <gst/check/gstcheck.h>
 #include <gst/gst.h>
 
 GType gst_assets_get_type(void);
 
 #define GST_TYPE_ASSETS (gst_assets_get_type())
-#define GST_ASSETS(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_ASSETS,GstAssets))
-#define GST_ASSETS_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_ASSETS,GstAssetsClass))
-#define GST_IS_ASSETS(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_ASSETS))
-#define GST_IS_ASSETS_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_ASSETS))
-
+#define GST_ASSETS(obj)                                                        \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_ASSETS, GstAssets))
+#define GST_ASSETS_CLASS(klass)                                                \
+    (G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_ASSETS, GstAssetsClass))
+#define GST_IS_ASSETS(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_ASSETS))
+#define GST_IS_ASSETS_CLASS(klass)                                             \
+    (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_ASSETS))
 
 typedef struct _GstAssets
 {
-    GstElement parent_instance;
-    GstElement *pipeline, *sink;
-    GstElement *dec;
-    GstPad *sinkpad;
-    GstBus *bus;
-    GMainLoop *loop;
+    GstBin parent_instance;
+    GstElement *internal_src;
 } GstAssets;
 
 typedef struct _GstAssetsClass
 {
-  GstElementClass parent_class;
+    GstBinClass parent_class;
 } GstAssetsClass;
-
 
 GST_ELEMENT_REGISTER_DECLARE(assets)
 
 #define ASSET_URI_STR "assets"
 
-static void
-gst_assets_set_property (GObject *object,
-                         guint prop_id,
-                         const GValue *value,
-                         GParamSpec *pspec)
+// static void gst_assets_set_property(GObject *object, guint prop_id,
+//                                     const GValue *value, GParamSpec *pspec)
+// {
+//     GstAssets *self = GST_ASSETS(object);
+//     switch (prop_id)
+//     {
+//     case 1:
+//         self->url_str = g_value_get_pointer(value);
+//         break;
+//     default:
+//         G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+//         break;
+//     }
+// }
+
+// static void gst_assets_get_property(GObject *object, guint prop_id,
+//                                     GValue *value, GParamSpec *pspec)
+// {
+//     GstAssets *self = GST_ASSETS(object);
+
+//     switch (prop_id)
+//     {
+//     case 1:
+//         g_value_set_pointer(value, self->url_str);
+//         break;
+//     default:
+//         G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+//         break;
+//     }
+// }
+
+static GstURIType gst_assets_uri_get_type(GType type) { return GST_URI_SRC; }
+
+static const gchar *const *gst_assets_uri_get_protocols(GType type)
 {
-  GstAssets *self = GST_ASSETS (object);
-  switch (prop_id) {
-    case 1: /* main-loop */
-        self->loop = g_value_get_pointer (value);
-        break;
-    case 2:
-        /* main-pipeline */
-        self->pipeline = g_value_get_pointer (value);
-        break;
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-      break;
-  }
+    static const gchar *protocols[] = {ASSET_URI_STR, NULL};
+
+    return protocols;
 }
 
-/* Called when someone uses g_object_get() */
-static void
-gst_assets_get_property (GObject *object,
-                         guint prop_id,
-                         GValue *value,
-                         GParamSpec *pspec)
+static gchar *gst_assets_uri_get_uri(GstURIHandler *handler)
 {
-  GstAssets *self = GST_ASSETS (object);
-
-  switch (prop_id) {
-    case 1:
-        g_value_set_pointer (value, self->loop);
-        break;
-    case 2:
-        g_value_set_pointer(value, self->pipeline);
-        break;
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-      break;
-  }
+    return g_strdup_printf("%s://", ASSET_URI_STR);
 }
 
+#include <stdio.h>
 
-
-static GstURIType
-gst_assets_uri_get_type (GType type)
+static gboolean gst_assets_uri_set_uri(GstURIHandler *handler, const gchar *uri,
+                                       GError **error)
 {
-  return GST_URI_SRC;
+    GstAssets *self = GST_ASSETS(handler);
+
+    gchar *path = gst_uri_get_location(uri);
+    if (!path)
+    {
+        g_set_error(error, GST_URI_ERROR, GST_URI_ERROR_BAD_URI,
+                    "Could not get location from URI: %s", uri);
+        return FALSE;
+    }
+    else if (!g_file_test(path, G_FILE_TEST_EXISTS))
+    {
+        g_set_error(error, GST_URI_ERROR, G_FILE_ERROR_NOENT,
+                    "File does not exist: %s", path);
+        g_free(path);
+        return FALSE;
+    }
+
+    // Build internal pipeline (filesrc)
+    if (self->internal_src)
+        gst_bin_remove(GST_BIN(self), self->internal_src);
+
+    self->internal_src = gst_element_factory_make("filesrc", NULL);
+    g_object_set(self->internal_src, "location", path, NULL);
+    gst_bin_add(GST_BIN(self), self->internal_src);
+
+    // Expose src pad
+    GstPad *srcpad = gst_element_get_static_pad(self->internal_src, "src");
+    GstPad *ghostpad = gst_ghost_pad_new("src", srcpad);
+    gst_pad_set_active(ghostpad, TRUE);
+    gst_element_add_pad(GST_ELEMENT(self), ghostpad);
+    gst_object_unref(srcpad);
+
+    g_free(path);
+    return TRUE;
 }
 
-static const gchar *const *
-gst_assets_uri_get_protocols (GType type)
+static void gst_assets_uri_handler_init(gpointer g_iface, gpointer iface_data)
 {
-  static const gchar *protocols[] = { ASSET_URI_STR, NULL };
+    GstURIHandlerInterface *iface = (GstURIHandlerInterface *)g_iface;
 
-  return protocols;
+    iface->get_type = gst_assets_uri_get_type;
+    iface->get_protocols = gst_assets_uri_get_protocols;
+    iface->get_uri = gst_assets_uri_get_uri;
+    iface->set_uri = gst_assets_uri_set_uri;
 }
 
-static gchar *
-gst_assets_uri_get_uri (GstURIHandler * handler)
+static void gst_assets_init(GstAssets *self)
 {
-  return g_strdup_printf ("%s://", ASSET_URI_STR);
+    ///
 }
 
-
-static gboolean
-gst_assets_uri_set_uri (GstURIHandler * handler, const gchar * uri, GError ** error)
+static void gst_assets_finalize(GObject *object)
 {
-  GstElement *self = GST_ELEMENT(handler);
+    GstAssets *self = GST_ASSETS(object);
 
-  if (!uri || !g_str_has_prefix(uri, "assets://"))
-    return FALSE;
-
-  return TRUE;
+    if (self->internal_src)
+    {
+        gst_bin_remove(GST_BIN(self), self->internal_src);
+        self->internal_src = NULL;
+    }
 }
 
-static void
-gst_assets_uri_handler_init (gpointer g_iface, gpointer iface_data)
+static void gst_assets_class_init(GstAssetsClass *self)
 {
-  GstURIHandlerInterface *iface = (GstURIHandlerInterface *) g_iface;
+    GstElementClass *element_class = GST_ELEMENT_CLASS(self);
+    GObjectClass *gobject_class = G_OBJECT_CLASS(self);
 
-  iface->get_type = gst_assets_uri_get_type;
-  iface->get_protocols = gst_assets_uri_get_protocols;
-  iface->get_uri = gst_assets_uri_get_uri;
-  iface->set_uri = gst_assets_uri_set_uri;
+    // gobject_class->set_property = gst_assets_set_property;
+    // gobject_class->get_property = gst_assets_get_property;
+    gobject_class->finalize = gst_assets_finalize;
+
+    // g_object_class_install_property(
+    //     gobject_class, 1,
+    //     g_param_spec_pointer("uri", "URI",
+    //                          "External or internal URI used by the plugin",
+    //                          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+
+    gst_element_class_set_static_metadata(
+        element_class, "Assets plugins proxy Poc", "Example/FirstExample",
+        "Goal to proxy custom URI", "Alexander Yanovskyy");
 }
 
-
-static void
-gst_assets_init(GstAssets * self)
+GType gst_assets_get_type(void)
 {
-  // gst_assets_src_init_type (GST_TYPE_ASSETS);
-}
+    static GType assets_type = 0;
 
-static void
-gst_assets_class_init (GstAssetsClass * self)
-{
-  GstElementClass *element_class = GST_ELEMENT_CLASS (self);
-  GObjectClass *gobject_class = G_OBJECT_CLASS (self);
+    if (!assets_type)
+    {
+        static const GTypeInfo assets_info = {
+            sizeof(GstAssetsClass),
+            NULL, /* base_init */
+            NULL, /* base_finalize */
+            (GClassInitFunc)gst_assets_class_init,
+            NULL, /* class_finalize */
+            NULL, /* class_data */
+            sizeof(GstAssets),
+            0, /* n_preallocs */
+            (GInstanceInitFunc)gst_assets_init,
+            NULL};
 
-  gobject_class->set_property = gst_assets_set_property;
-  gobject_class->get_property = gst_assets_get_property;
+        static const GInterfaceInfo uri_hdlr_info = {
+            gst_assets_uri_handler_init, NULL, NULL};
 
-  g_object_class_install_property (gobject_class, 1,
-    g_param_spec_pointer ("main-loop",
-                          "Main Loop",
-                          "External or internal GMainLoop used by the plugin",
-                          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+        assets_type =
+            g_type_register_static(GST_TYPE_BIN, "GstAssets", &assets_info, 0);
+        g_type_add_interface_static(assets_type, GST_TYPE_URI_HANDLER,
+                                    &uri_hdlr_info);
+    }
 
-
-  g_object_class_install_property (gobject_class, 2,
-    g_param_spec_pointer ("main-pipeline",
-                          "Main Pipeline",
-                          "External or internal GstPipeline used by the plugin",
-                          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
-
-
-  gst_element_class_set_static_metadata (element_class,
-    "Assets plugins proxy Poc",
-    "Example/FirstExample",
-    "Goal to proxy custom URI",
-    "Alexander Yanovskyy");
-
-}
-
-
-GType 
-gst_assets_get_type(void) 
-{
-  static GType assets_type = 0;
-
-  if (!assets_type) {
-    static const GTypeInfo assets_info = {
-      sizeof (GstAssetsClass),
-      NULL,   /* base_init */
-      NULL,   /* base_finalize */
-      (GClassInitFunc) gst_assets_class_init,
-      NULL,   /* class_finalize */
-      NULL,   /* class_data */
-      sizeof (GstAssets),
-      0,      /* n_preallocs */
-      (GInstanceInitFunc) gst_assets_init,
-      NULL
-    };
-
-    static const GInterfaceInfo uri_hdlr_info = {
-      gst_assets_uri_handler_init, NULL, NULL
-    };
-
-    assets_type = g_type_register_static (GST_TYPE_ELEMENT,
-        "GstAssets", &assets_info, 0);
-    g_type_add_interface_static (assets_type, GST_TYPE_URI_HANDLER,
-        &uri_hdlr_info);  
-
-  }
-
-  return assets_type;
+    return assets_type;
 }
--- gst_custom_uri.c
+++ gst_custom_uri.c
@@ -1,182 +1,162 @@
-#include <gst/check/gstcheck.h>
-#include <gst/base/gstpushsrc.h>
-#include <gst/gst.h>
 #include "gst_assets.h"
+#include <gst/base/gstpushsrc.h>
+#include <gst/check/gstcheck.h>
+#include <gst/gst.h>
 
 // https://gstreamer.freedesktop.org/documentation/tutorials/basic/dynamic-pipelines.html?gi-language=c
 
-
-// G_DEFINE_TYPE (GstAssets, gst_assets, GST_TYPE_ELEMENT);
+// G_DEFINE_TYPE(GstAssets, gst_assets, GST_TYPE_ELEMENT);
 
 GST_ELEMENT_REGISTER_DEFINE(assets, "assets", GST_RANK_NONE, GST_TYPE_ASSETS);
 
-
-
-static gboolean
-my_bus_callback (GstBus * bus, GstMessage * message, gpointer data)
+static gboolean plugin_init(GstPlugin *plugin)
 {
-  GMainLoop *loop = (GMainLoop *)data;
-  // g_print ("Got %s message\n", GST_MESSAGE_TYPE_NAME (message));
-  GError *err;
-  gchar *debug;
-  switch (GST_MESSAGE_TYPE (message)) {
-    case GST_MESSAGE_ERROR:{
-      gst_message_parse_error (message, &err, &debug);
-      g_print ("Error: %s\n", err->message);
-      g_error_free (err);
-      g_free (debug);
-
-      g_main_loop_quit (loop);
-      break;
-    }
-    case GST_MESSAGE_WARNING:{
-      gst_message_parse_warning (message, &err, &debug);
-      g_print ("Warning: %s\n", err->message);
-      g_error_free (err);
-      g_free (debug);
-      break;
-    }
-    case GST_MESSAGE_EOS:
-      /* end-of-stream */
-      g_main_loop_quit (loop);
-      break;
-    default:
-      /* unhandled message */
-      break;
-  }
-
-  /* we want to be notified again the next time there is a message
-   * on the bus, so returning TRUE (FALSE means we want to stop watching
-   * for messages on the bus and our callback should not be called again)
-   */
-  return TRUE;
-}
-
-static void
-cb_pad_added (GstElement *dec,
-          GstPad     *pad,
-          gpointer    data)
-{
-  GstElement *ssink = (GstElement*)data;
-  GstCaps *caps;
-  GstStructure *str;
-  const gchar *name;
-  GstPadTemplate *templ;
-  GstElementClass *klass;
-
-  /* check media type */
-  caps = gst_pad_query_caps (pad, NULL);
-  str = gst_caps_get_structure (caps, 0);
-  name = gst_structure_get_name (str);
-
-  klass = GST_ELEMENT_GET_CLASS (ssink);
-
-  if (g_str_has_prefix (name, "audio")) {
-    templ = gst_element_class_get_pad_template (klass, "audio_sink");
-  } else if (g_str_has_prefix (name, "video")) {
-    templ = gst_element_class_get_pad_template (klass, "video_sink");
-  } else if (g_str_has_prefix (name, "text")) {
-    templ = gst_element_class_get_pad_template (klass, "text_sink");
-  } else {
-    templ = NULL;
-  }
-
-  if (templ) {
-    GstPad *sinkpad;
-
-    sinkpad = gst_element_request_pad (ssink, templ, NULL, NULL);
-
-    if (!gst_pad_is_linked (sinkpad))
-      gst_pad_link (pad, sinkpad);
-
-    gst_object_unref (sinkpad);
-  }
-
-  gst_clear_caps (&caps);
-}
-
-
-
-
-
-
-static gboolean
-plugin_init (GstPlugin *plugin)
-{
-  return GST_ELEMENT_REGISTER (assets, plugin);
+    return GST_ELEMENT_REGISTER(assets, plugin);
 }
 
 #define PACKAGE "AssetsPoC"
 #define VERSION "1.0"
-GST_PLUGIN_DEFINE (
-  GST_VERSION_MAJOR,
-  GST_VERSION_MINOR,
-  assets,
-  "Plugin proxy PoC",
-  plugin_init,
-  VERSION,
-  "LGPL",
-  "GStreamer",
-  "http://gstreamer.net/"
-)
+GST_PLUGIN_DEFINE(GST_VERSION_MAJOR, GST_VERSION_MINOR, assets,
+                  "Plugin proxy PoC", plugin_init, VERSION, "LGPL", "GStreamer",
+                  "http://gstreamer.net/")
 
+static void cb_pad_added(GstElement *dec, GstPad *pad, gpointer data)
+{
+    GstElement *ssink = (GstElement *)data;
+    GstCaps *caps;
+    GstStructure *str;
+    const gchar *name;
+    GstPadTemplate *templ;
+    GstElementClass *klass;
 
+    /* check media type */
+    caps = gst_pad_query_caps(pad, NULL);
+    str = gst_caps_get_structure(caps, 0);
+    name = gst_structure_get_name(str);
 
+    klass = GST_ELEMENT_GET_CLASS(ssink);
 
-gint
-main (gint   argc,
-      gchar *argv[])
+    if (g_str_has_prefix(name, "audio"))
+    {
+        templ = gst_element_class_get_pad_template(klass, "audio_sink");
+    }
+    else if (g_str_has_prefix(name, "video"))
+    {
+        templ = gst_element_class_get_pad_template(klass, "video_sink");
+    }
+    else if (g_str_has_prefix(name, "text"))
+    {
+        templ = gst_element_class_get_pad_template(klass, "text_sink");
+    }
+    else
+    {
+        templ = NULL;
+    }
+
+    if (templ)
+    {
+        GstPad *sinkpad;
+
+        sinkpad = gst_element_request_pad(ssink, templ, NULL, NULL);
+
+        if (!gst_pad_is_linked(sinkpad))
+            gst_pad_link(pad, sinkpad);
+
+        gst_object_unref(sinkpad);
+    }
+
+    gst_clear_caps(&caps);
+}
+
+static gboolean my_bus_callback(GstBus *bus, GstMessage *message, gpointer data)
+{
+    GMainLoop *loop = (GMainLoop *)data;
+    GError *err;
+    gchar *debug;
+    switch (GST_MESSAGE_TYPE(message))
+    {
+    case GST_MESSAGE_ERROR:
+    {
+        gst_message_parse_error(message, &err, &debug);
+        g_print("Error: %s\n", err->message);
+        g_error_free(err);
+        g_free(debug);
+
+        g_main_loop_quit(loop);
+        break;
+    }
+    case GST_MESSAGE_WARNING:
+    {
+        gst_message_parse_warning(message, &err, &debug);
+        g_print("Warning: %s\n", err->message);
+        g_error_free(err);
+        g_free(debug);
+        break;
+    }
+    case GST_MESSAGE_EOS:
+        /* end-of-stream */
+        g_main_loop_quit(loop);
+        break;
+    default:
+        /* unhandled message */
+        break;
+    }
+
+    /* we want to be notified again the next time there is a message
+     * on the bus, so returning TRUE (FALSE means we want to stop watching
+     * for messages on the bus and our callback should not be called again)
+     */
+    return TRUE;
+}
+
+gint main(gint argc, gchar *argv[])
 {
 
-  /* init GStreamer */
-  gst_init (&argc, &argv);
+    gst_init(&argc, &argv);
 
-  if (!gst_plugin_register_static(GST_VERSION_MAJOR, GST_VERSION_MINOR,
-  "assetsrc", "Red Video Source", plugin_init,
-  "1.0", "LGPL", "GStreamer", "http://gstreamer.net/", "source"))
-  {
-      g_printerr("Failed to register assetsrc plugin\n");
-      return -1;
-  }
+    if (!gst_plugin_register_static(GST_VERSION_MAJOR, GST_VERSION_MINOR,
+                                    "assetsrc", "Red Video Source", plugin_init,
+                                    "1.0", "LGPL", "GStreamer",
+                                    "http://gstreamer.net/", "source"))
+    {
+        g_printerr("Failed to register assetsrc plugin\n");
+        return -1;
+    }
 
+    if (argc != 2)
+    {
+        g_print("Usage: %s <uri>\n", argv[0]);
+        return -1;
+    }
 
-  /* make sure we have input */
-  if (argc != 2) {
-    g_print ("Usage: %s <uri>\n", argv[0]);
-    return -1;
-  }
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
 
-  GMainLoop *loop = g_main_loop_new (NULL, FALSE);
+    if (argc != 2)
+    {
+        g_print("Usage: %s <uri>\n", argv[0]);
+        return -1;
+    }
 
+    GstElement *pipeline = gst_pipeline_new("pipeline");
+    GstElement *sink = gst_element_factory_make("playsink", "sink");
 
-  GstElement *pipeline = gst_pipeline_new ("pipeline");
-  GstBus *bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-  gst_bus_add_watch (bus, my_bus_callback, loop);
-  gst_object_unref (bus);
+    GstBus *bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+    gst_bus_add_watch(bus, my_bus_callback, loop);
+    gst_object_unref(bus);
 
-  GstElement *sink = gst_element_factory_make("playsink", "sink");
+    GstElement *uridecodebin =
+        gst_element_factory_make("uridecodebin", "source");
+    g_object_set(G_OBJECT(uridecodebin), "uri", argv[1], NULL);
+    g_signal_connect(uridecodebin, "pad-added", G_CALLBACK(cb_pad_added), sink);
 
-  GstElement *source = gst_element_factory_make("uridecodebin", "source");
-  g_object_set (G_OBJECT (source), "uri", argv[1], NULL);
-  g_signal_connect (source, "pad-added", G_CALLBACK (cb_pad_added), sink);
-  
-  gst_bin_add_many (GST_BIN (pipeline), source, sink, NULL);
+    gst_bin_add_many(GST_BIN(pipeline), uridecodebin, sink, NULL);
 
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    g_main_loop_run(loop);
 
-  GstElement *src = gst_element_factory_make("assets", NULL);
-  g_object_set (G_OBJECT(src), "main-loop", loop, NULL);
-  g_object_set (G_OBJECT(src), "main-pipeline", pipeline, NULL);
-
-
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
-  g_main_loop_run (loop);
-
-
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (GST_OBJECT (pipeline));
-
-
-  return 0;
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(GST_OBJECT(pipeline));
+    return 0;
 }
 
 // https://gstreamer.freedesktop.org/documentation/application-development/highlevel/playback-components.html?gi-language=c
